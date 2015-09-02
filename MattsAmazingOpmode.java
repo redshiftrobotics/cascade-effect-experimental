be35ca306ffbf5778ca5c790af3a09fa09d1057c
Matt <mattkelseymk@gmail.com>
2015-09-02 16:24:10 -0700

    Add nonlinear joystick input scaling

--- MattsAmazingOpmode.java
+++ MattsAmazingOpmode.java
@@ -1,58 +1,126 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-
-public class MattsAmazingOpmode extends OpMode
+//------------------------------------------------------------------------------
+//
+// PushBotManual
+//
+/**
+ * Extends the PushBotTelemetry and PushBotHardware classes to provide a basic
+ * manual operational mode for the Push Bot.
+ *
+ * @author SSI Robotics
+ * @version 2015-08-01-06-01
+ */
+public class PushBotManual extends PushBotTelemetry
 
 {
-    //declare motor controller
-    private DcMotorController v_dc_motor_controller_drive;
-
-    //declare variables for left and right motors
-    private DcMotor v_motor_left_drive;
-    final int v_channel_left_drive = 1;
-
-    private DcMotor v_motor_right_drive;
-    final int v_channel_right_drive = 2;
-
-    @Override public void init ()
-
-    {
-        //this is where hardware interfaces are initialized and bound
-        v_dc_motor_controller_drive = hardwareMap.dcMotorController.get("drive_controller");
-
-        v_motor_left_drive = hardwareMap.dcMotor.get ("left_drive");
-
-        v_motor_right_drive = hardwareMap.dcMotor.get ("right_drive");
-
-
-    }
-
-    @Override public void start ()
+    //--------------------------------------------------------------------------
+    //
+    // PushBotManual
+    //
+    //--------
+    // Constructs the class.
+    //
+    // The system calls this member when the class is instantiated.
+    //--------
+    public PushBotManual ()
 
     {
-       //Actions in this method will execute before the 'Start' button is pressed on the controller, its the equivalent of the setup loop in Arduino programming.
+        //
+        // Initialize base classes.
+        //
+        // All via self-construction.
 
-    }
+        //
+        // Initialize class members.
+        //
+        // All via self-construction.
 
-    public MattsAmazingOpmode()
+    } // PushBotManual::PushBotManual
 
-    {
-
-    }
-
+    //--------------------------------------------------------------------------
+    //
+    // loop
+    //
+    //--------
+    // Initializes the class.
+    //
+    // The system calls this member repeatedly while the OpMode is running.
+    //--------
     @Override public void loop ()
 
     {
-        //Again the equivalent in Arduino programming is the loop()... This will continue to execute (in this case in a linear/synchronous fashion) until stop() is called (at least I think thats how you terminate it).
+        //----------------------------------------------------------------------
+        //
+        // DC Motors
+        //
+        // Obtain the current values of the joystick controllers.
+        //
+        // Note that x and y equal -1 when the joystick is pushed all of the way
+        // forward (i.e. away from the human holder's body).
+        //
+        // The clip method guarantees the value never exceeds the range +-1.
+        //
+        // The DC motors are scaled to make it easier to control them at slower
+        // speeds.
+        //
+        // The setPower methods write the motor power values to the DcMotor
+        // class, but the power levels aren't applied until this method ends.
+        //
 
+        //
+        // Manage the drive wheel motors.
+        //
+        float l_gp1_left_stick_y = -gamepad1.left_stick_y;
+        float l_left_drive_power
+            = (float)scale_motor_power (l_gp1_left_stick_y);
 
-        //This sets the drive power of both motors to the first gamepads joysticks.
-        v_motor_left_drive.setPower (-gamepad1.left_stick_y);
-        v_motor_right_drive.setPower (-gamepad1.right_stick_y);
+        float l_gp1_right_stick_y = -gamepad1.right_stick_y;
+        float l_right_drive_power
+            = (float)scale_motor_power (l_gp1_right_stick_y);
 
-    }
+        set_drive_power (l_left_drive_power, l_right_drive_power);
 
-}
+        //
+        // Manage the arm motor.
+        //
+        float l_gp2_left_stick_y = -gamepad2.left_stick_y;
+        float l_left_arm_power = (float)scale_motor_power (l_gp2_left_stick_y);
+        v_motor_left_arm.setPower (l_left_arm_power);
+
+        //----------------------------------------------------------------------
+        //
+        // Servo Motors
+        //
+        // Obtain the current values of the gamepad 'x' and 'b' buttons.
+        //
+        // Note that x and b buttons have boolean values of true and false.
+        //
+        // The clip method guarantees the value never exceeds the allowable range of
+        // [0,1].
+        //
+        // The setPosition methods write the motor power values to the Servo
+        // class, but the positions aren't applied until this method ends.
+        //
+        if (gamepad2.x)
+        {
+            m_hand_position (a_hand_position () + 0.05);
+        }
+        else if (gamepad2.b)
+        {
+            m_hand_position (a_hand_position () - 0.05);
+        }
+
+        //
+        // Send telemetry data to the driver station.
+        //
+        update_telemetry (); // Update common telemetry
+        telemetry.addData ("10", "GP1 Left: " + l_gp1_left_stick_y);
+        telemetry.addData ("11", "GP1 Right: " + l_gp1_right_stick_y);
+        telemetry.addData ("12", "GP2 Left: " + l_gp2_left_stick_y);
+        telemetry.addData ("13", "GP2 X: " + gamepad2.x);
+        telemetry.addData ("14", "GP2 Y: " + gamepad2.b);
+
+    } // PushBotManual::loop
+
+} // PushBotManual
